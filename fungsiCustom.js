85e5e9e364cdea85bc99168b07dfba5a0dfb727a
Dimas Maulana Ahsan <dimasmaulanaahsan@gmail.com>
2022-06-11 08:09:35 +0700

    Add bacaData function

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,42 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let hasil = [];
+
+  fs.readFile(file1, "utf8", (err, data) => {
+    if (err) {
+      fnCallback(err, null);
+    }
+
+    let json = JSON.parse(data);
+    let split = json.message.split(" ");
+
+    hasil.push(split[1]);
+    fs.readFile(file2, "utf8", (err, data) => {
+      if (err) {
+        fnCallback(err, null);
+      }
+
+      let json = JSON.parse(data);
+      let split = json[0].message.split(" ");
+
+      hasil.push(split[1]);
+      fs.readFile(file3, "utf8", (err, data) => {
+        if (err) {
+          fnCallback(err, null);
+        }
+
+        let json = JSON.parse(data);
+        let split = json[0].data.message.split(" ");
+
+        hasil.push(split[1]);
+
+        fnCallback(null, hasil);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
